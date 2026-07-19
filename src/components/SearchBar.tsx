e21276c826a324b321ec3e9318d28465fb4561fd
marionmartie <148829408+marionmartie@users.noreply.github.com>
2026-07-19 23:03:57 +0800

    Day 6 - Added basic form function and validation.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,37 @@
 import { Search } from "lucide-react"
+import { useState, type ChangeEvent, type SubmitEvent } from "react"
+
+const SearchButton = () => {
+    return (
+        <button type="submit" className="bg-brand-primary p-2 rounded-sm">
+            <Search color="white" />
+        </button>
+    )
+}
 
 const SearchBar = () => {
+    const [searchInput, setSearchInput] = useState<string>("")
+    const [isFormValid, setIsFormValid] = useState<boolean>(true)
+    const [formMessage, setFormMessage] = useState<string>("")
+
+    const handleSubmit = (e:SubmitEvent) => {
+        e.preventDefault()
+        if (searchInput.trim() === "") {
+            setIsFormValid( false )
+            setFormMessage( "Please enter a name!" )
+            return false
+        }
+        console.log(searchInput)
+    }
+
     return (
-        <div className="max-w-5xl mx-auto px-8 my-8 flex gap-2">
-            <input type="text" className="bg-white p-2 rounded-sm w-full" />
+        <form onSubmit={handleSubmit} className="max-w-5xl mx-auto px-8 my-8 flex gap-2 flex-wrap">
+            <input value={searchInput} onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchInput(e.target.value)} type="text" className="bg-white p-2 rounded-sm w-2/3" />
             <label htmlFor="" className="sr-only">Search Bar</label>
-            <button className="bg-brand-primary p-2 rounded-sm">
-                <Search color="white" />
-            </button>
-        </div>
+            <SearchButton />
+            
+            {!isFormValid && <p className="w-full text-text-contrast">{ formMessage }</p>}
+        </form>
     )
 }
 
